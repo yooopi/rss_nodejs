97bda50e230cf7d50f6f3fde519c63d289137abb
Vasak Aramian <yooopi.av@gmail.com>
2021-11-18 01:47:09 +0500

    refactor: add human friendly error handler

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
+const fs = require("fs");
 const { pipeline } = require("stream");
-const fs = require('fs');
 const { readable, writable } = require("./utils/streamsGenerator");
 const { argumentsParser } = require("./utils/argumentsParser");
 const { getStreamsFromConfig } = require("./utils/getStreamsArrFromConfig");
-const { input, output, config } = argumentsParser();
+const { errorsHandler } = require("./utils/errorsHandler");
 
-const streamsArr = getStreamsFromConfig(config.split("-"));
-// const writable = fs.createWriteStream(output, { options: "a+", encoding: "utf-8" })
-
-pipeline(readable(input), ...streamsArr, writable(output), (err) => {
-  if (err) {
-    throw new Error(err.message);
-  }
-});
+try {
+  const { input, output, config } = argumentsParser();
+  const streamsArr = getStreamsFromConfig(config.split("-"));
+  
+  pipeline(readable(input), ...streamsArr, writable(output), (err) => {
+    if (err) throw new Error("Unknown eror");
+  });
+} catch (err) {
+  errorsHandler(err);
+}
---
+++ utils/errorsHandler.js
@@ -0,0 +1,8 @@
+// Return human friendly error
+
+const process = require("process");
+
+exports.errorsHandler = (err) => {
+  process.exitCode = 1;
+  process.stderr.write(err.message + '\n');
+};
