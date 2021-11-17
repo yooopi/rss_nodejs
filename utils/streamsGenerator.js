d363865b9eb063a0edbdcc82a68150e94fd429b7
Vasak Aramian <yooopi.av@gmail.com>
2021-11-18 00:56:20 +0500

    fix: error throwing in stream generator while user passed non-existent path

--- utils/streamsGenerator.js
+++ utils/streamsGenerator.js
@@ -1,28 +1,31 @@
 const fs = require("fs");
 const { constants } = require("fs");
+const path = require("path");
 const { stdin, stdout } = require("process");
 const { Transform } = require("stream");
 const { atbash, caesar, rot8 } = require("../ciphers");
 const { CipherModerError, FileError } = require("./errors");
 
 exports.readable = (path) => {
-  if (path == undefined || !path) return stdin;
+  try {
+    if (path === undefined || !path) return stdin;
 
-  fs.access(path, constants.F_OK | constants.R_OK, (err) => {
-    if (err) throw new FileError(path);
-  });
-
-  return fs.createReadStream(path, { options: "r", encoding: "utf-8" });
+    fs.accessSync(path, constants.F_OK | constants.R_OK);
+    return fs.createReadStream(path, { options: "r", encoding: "utf-8" });
+  } catch {
+    throw new FileError(path);
+  }
 };
 
 exports.writable = (path) => {
-  if (path === undefined || !path) return stdout;
+  try {
+    if (path === undefined || !path) return stdout;
 
-  fs.access(path, constants.F_OK | constants.W_OK, (err) => {
-    if (err) throw new FileError(path);
-  });
-  
-  return fs.createWriteStream(path, { flags: "a+", encoding: "utf-8" });
+    fs.accessSync(path, constants.F_OK | constants.W_OK);
+    return fs.createWriteStream(path, { flags: "a+", encoding: "utf-8" });
+  } catch {
+    throw new FileError(path);
+  }
 };
 
 exports.atbash = () =>
